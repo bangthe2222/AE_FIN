43747bc3f16413aec9994fe865a5ddf72dbfb92c
bangthe2222 <bangthedz2222@gmail.com>
2024-02-27 12:06:30 +0700

    update PSR

--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,8 @@
+# -*- coding: utf-8 -*-
 import torch
 import numpy as np
 import pandas as pd
+import scipy.stats as scipy_stats
 import matplotlib.pyplot as plt 
 import seaborn as sns
 from torch.utils.data import DataLoader
@@ -235,3 +237,43 @@ class EDAProcess:
             sns.boxplot(df.iloc[:, min:])
         else:
             sns.boxplot(df.iloc[:, min:max])
+
+
+
+def skew_to_alpha(skew):
+    """
+    Convert a skew to alpha parameter needed by scipy_stats.skewnorm(..).
+
+    Parameters
+    ----------
+    skew: float
+        Must be between [-0.999, 0.999] for avoiding complex numbers.
+
+    Returns
+    -------
+    float
+    """
+    d = (np.pi / 2 * ((abs(skew) ** (2 / 3)) / (abs(skew) ** (2 / 3) + ((4 - np.pi) / 2) ** (2 / 3)))) ** 0.5
+    a = (d / ((1 - d ** 2) ** .5))
+    return a * np.sign(skew)
+
+
+def moments(returns):
+    """
+    Calculate the four moments: mean, std, skew, kurtosis.
+
+    Parameters
+    ----------
+    returns: np.array, pd.Series, pd.DataFrame
+
+    Returns
+    -------
+    pd.Series, pd.DataFrame
+    """
+    if type(returns) != pd.DataFrame:
+        return pd.Series({'mean': np.mean(returns),
+                          'std': np.std(returns, ddof=1),
+                          'skew': scipy_stats.skew(returns),
+                          'kurt': scipy_stats.kurtosis(returns, fisher=False)})
+    else:
+        return returns.apply(moments, axis=1)
